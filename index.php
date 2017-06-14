9e9718053c230d3b4c51a08f873fd20fd00c7ac9
hafizjoundy <hafizjoundys@gmail.com>
2017-06-14 18:48:26 +0700

    bms

---
+++ index.php
@@ -0,0 +1,13 @@
+<center>
+
+Book My Show Ganteng <br><br>
+<?php
+function bmsGenerate(){
+  return rand(1000000,8000000);
+}
+for ($i=0; $i < 30; $i++) {
+  echo "7".bmsGenerate()."<br>";
+}
+?>
+
+</center>
